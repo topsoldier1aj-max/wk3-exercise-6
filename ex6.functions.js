b734afb7ee890940aae545b8a7d6402694e9a33b
topsoldier1aj-max <topsoldier1aj@gmail.com>
2025-11-13 10:05:09 +0200

    week 3 exercie 6

---
+++ ex6.functions.js
@@ -0,0 +1,13 @@
+function greet(name){
+    return (`Welcome ${name} !`)
+}
+
+function add(a,b){
+    return a + b
+}
+
+let greeting = greet('Michael');
+let sum = add(5,4);
+
+console.log(greeting);
+console.log(sum);
